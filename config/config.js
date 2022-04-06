0b225de7b873b662c470c2eb242a41bde06ff85b
Poorna-Athreya <poorna_athreya@mckinsey.com>
2022-04-06 18:57:07 +0530

    Chore: Creating the  docker file and docker-compose

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   test: {
